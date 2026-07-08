2d41c4cc195e210433c0fdb0dd3ba89d3827374c
Kunal Mukherjee <kunal.mukherjee2205@gmail.com>
2026-07-08 10:33:13 +0530

    Update auth_routes.py

--- backend/routes/auth_routes.py
+++ backend/routes/auth_routes.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 import bcrypt
-from backend.extensions import db
-from backend.models.user import User
+from extensions import db
+from models.user import User
 
 bp = Blueprint("auth", __name__, url_prefix="/api/auth")
 
